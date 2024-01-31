aed35c508378139c85ad71387a2e778594b0ca75
WalterSumbon <45062253+WalterSumbon@users.noreply.github.com>
2024-01-31 20:35:15 +0800

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="easyjailbreak",
-    version="0.1.0",
+    version="0.1.1",
     description="Easy Jailbreak toolkit",
     author="ZenithX",
     author_email="waltersumbon@gmail.com",
