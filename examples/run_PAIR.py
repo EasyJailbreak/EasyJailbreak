84fb5daf603e8fb10607308af3430f53f8535875
Temoor Ali <temoor556@gmail.com>
2024-10-14 10:40:31 +0300

    Add updated run_PAIR that uses deepseek and llama, with harmbench

--- examples/run_PAIR.py
+++ examples/run_PAIR.py
@@ -1,16 +1,24 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+from easyjailbreak.attacker.AutoDAN_Liu_2023 import AutoDAN
 from easyjailbreak.attacker.PAIR_chao_2023 import PAIR
 from easyjailbreak.datasets import JailbreakDataset
 from easyjailbreak.models.huggingface_model import from_pretrained
 from easyjailbreak.models.openai_model import OpenaiModel
 
 # First, prepare models and datasets.
-attack_model = from_pretrained(model_name_or_path='lmsys/vicuna-13b-v1.5',
-                               model_name='vicuna_v1.1')
-target_model = OpenaiModel(model_name='gpt-4',
-                         api_keys='INPUT YOUR KEY HERE!!!')
-eval_model = OpenaiModel(model_name='gpt-4',
-                         api_keys='INPUT YOUR KEY HERE!!!')
-dataset = JailbreakDataset('AdvBench')
+BASE_URL = "http://localhost:2213/v1"
+
+# example target model 
+target_model = OpenaiModel(model_name="meta-llama/Llama-3.2-3B-Instruct", api_keys="EMPTY", base_url=BASE_URL)
+
+# 加载attack model
+attack_model = OpenaiModel(model_name="deepseek-chat", api_keys="", base_url="https://api.deepseek.com/")
+
+eval_model = OpenaiModel(model_name="meta-llama/Llama-3.2-3B-Instruct", api_keys="EMPTY", base_url=BASE_URL)
+
+dataset = JailbreakDataset('HarmBench')
 
 # Then instantiate the recipe.
 attacker = PAIR(attack_model=attack_model,
